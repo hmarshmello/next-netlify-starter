01531f6b5ce69cbb5d45b75ae6d8bd52bd173864
Harsh SHRIVASTAVA <62792682+hmarshmello@users.noreply.github.com>
2022-02-02 01:07:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Portfolio!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my Portfolio!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
